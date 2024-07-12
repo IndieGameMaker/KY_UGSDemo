fb511be1050071819c78e4eae692c731daea3402
Lee JaeHyun <myleje@gmail.com>
2024-07-12 13:43:20 +0900

    Project Init

---
+++ Assets/02_Scripts/AuthManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.Services.Core;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+
+public class AuthManager : MonoBehaviour
+{
+    [SerializeField] private Button signInButton;
+    [SerializeField] private TMP_Text messageText;
+
+    private async void Awake()
+    {
+        // UGS 초기화
+        await UnityServices.InitializeAsync();
+
+        // 버튼 이벤트 연결
+        signInButton.onClick.AddListener(async () =>
+        {
+            // 익명 로그인 메소드 호출
+            await SignInAsync();
+        });
+    }
+
+    // 익명 로그인
+    private async Task SignInAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("익명 사용자 로그인 완료");
+        }
+        catch (AuthenticationException e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+}
