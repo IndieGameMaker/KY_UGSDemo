b4398b18c00c1af518a4cf49a9af1e8801cd806d
Lee JaeHyun <myleje@gmail.com>
2024-07-19 15:15:32 +0900

    .

---
+++ Assets/02_Scripts/RCManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+
+public class RCManager : MonoBehaviour
+{
+    private async Task Awake()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+}
