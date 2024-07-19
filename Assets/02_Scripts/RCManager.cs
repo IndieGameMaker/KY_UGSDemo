0a730fc96749eb109000bee8f430a071fbfa122e
Lee JaeHyun <myleje@gmail.com>
2024-07-19 15:27:09 +0900

    .

--- Assets/02_Scripts/RCManager.cs
+++ Assets/02_Scripts/RCManager.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using Unity.Services.RemoteConfig;
 using UnityEngine;
 
 public class RCManager : MonoBehaviour
 {
+    [SerializeField] private float mummyScale;
+
     private async Task Awake()
     {
         await UnityServices.InitializeAsync();
@@ -15,7 +19,25 @@ public class RCManager : MonoBehaviour
         {
             Debug.Log("Player ID :" + AuthenticationService.Instance.PlayerId);
         };
-
+        // 로그인
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        // Remote Config 이벤트 연결
+        RemoteConfigService.Instance.FetchCompleted += (response) =>
+        {
+            mummyScale = RemoteConfigService.Instance.appConfig.GetFloat("mummy_scale");
+            Debug.Log("Mummy Scale :" + mummyScale);
+        };
+
+        // 리모트 값 조회
+        await GetRemoteConfigData();
+    }
+
+    public struct userAttributes { };
+    public struct appAttributes { };
+
+    private async Task GetRemoteConfigData()
+    {
+        await RemoteConfigService.Instance.FetchConfigsAsync(new userAttributes(), new appAttributes());
     }
 }
