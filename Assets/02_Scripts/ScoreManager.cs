b362a87d0655b852dd78b9975058305483743ec4
Lee JaeHyun <myleje@gmail.com>
2024-07-19 13:46:22 +0900

    .

--- Assets/02_Scripts/ScoreManager.cs
+++ Assets/02_Scripts/ScoreManager.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     private string playerId;
+
+    [Header("UI Setting")]
+    [SerializeField] private TMP_InputField scoreIf;
+    [SerializeField] private Button scoreSaveButton;
 
     private async void Awake()
     {
